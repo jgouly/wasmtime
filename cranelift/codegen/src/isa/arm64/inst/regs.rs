bf460797694588d0b917c6eab7b86ad441a9b9e7
Benjamin Bouvier <public@benj.me>
2020-03-24 13:59:07 +0100

    arm64: use x13 as the scratch register, and not the VMContext reg x14;

--- cranelift/codegen/src/isa/arm64/inst/regs.rs
+++ cranelift/codegen/src/isa/arm64/inst/regs.rs
@@ -183,7 +183,7 @@ pub fn create_reg_universe() -> RealRegUniverse {
     allocable_by_class[RegClass::I64.rc_to_usize()] = Some(RegClassInfo {
         first: x_reg_base as usize,
         last: x_reg_last as usize,
-        suggested_scratch: Some(XREG_INDICES[14] as usize),
+        suggested_scratch: Some(XREG_INDICES[13] as usize),
     });
     allocable_by_class[RegClass::V128.rc_to_usize()] = Some(RegClassInfo {
         first: v_reg_base as usize,
